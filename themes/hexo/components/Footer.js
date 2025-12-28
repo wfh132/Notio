3337beca649980db866dd333cf678506b2d70b98
wfh132 <104284186+wfh132@users.noreply.github.com>
2025-12-28 16:17:28 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -35,7 +35,6 @@ const Footer = ({ title }) => {
         <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>
           {title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}
         </h1>
-        <PoweredBy className='justify-center' />
       </span>
       <br />
     </footer>
